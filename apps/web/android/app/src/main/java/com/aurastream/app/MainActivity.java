9fedc0b19e7946bd7662b821653710138b25ecca
“Karolu <Manogm2005@gmail.com>
2026-06-26 11:24:36 +0530

    Fix: MainActivity public method signatures for Java compilation

--- apps/web/android/app/src/main/java/com/aurastream/app/MainActivity.java
+++ apps/web/android/app/src/main/java/com/aurastream/app/MainActivity.java
@@ -3,30 +3,26 @@ package com.aurastream.app;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Enable hardware acceleration
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            WebView.setWebContentsDebuggingEnabled(false);
-        }
-
-        // Start background audio service
         startAudioService();
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
-        // Re-acquire audio focus on resume
         startAudioService();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this, AudioService.class));
     }
 
     private void startAudioService() {
@@ -36,12 +32,5 @@ public class MainActivity extends BridgeActivity {
         } else {
             startService(serviceIntent);
         }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // Stop service only when app is fully closed
-        stopService(new Intent(this, AudioService.class));
     }
 }
